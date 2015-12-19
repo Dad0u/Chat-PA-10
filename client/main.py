b4e6ef9323bafcf7a72f9273eee97b3ae3301191
Victor Couty <victor@couty.eu>
2015-12-19 18:12:38 +0100

    Ajout du GUI

--- client/main.py
+++ client/main.py
@@ -1,15 +1,16 @@
-#coding: utf-8
+# -*- coding:utf-8 -*-
 
 from threading import Thread
 import socket
 from time import *
 import sys
 import select
+import tkinter as tk
 
 PORT = 1148
 SIZE = 2048
 
-class Getter(Thread):
+class Fetcher(Thread):
     def __init__(self,conn):
         Thread.__init__(self)
         self.continuer = True
@@ -17,27 +18,70 @@ class Getter(Thread):
     def run(self):
         while self.continuer:
             demande = select.select([self.conn],[],[],0.2)[0]
-            print("lulu")
-            print(demande)
             if len(demande) == 0:
-                print("OK")
                 continue
-            print("HO!")
             msg = self.conn.recv(SIZE).decode()
             print("Commande du serveur: "+msg)
             if len(msg) == 0:
-                print("reçu du vide, sortie du getter")
                 self.stop()
             if msg[:3] == "MSG":
-                l = msg[3:].split("\\")
+                l = msg[4:].split("\\")
                 pseudo = l[0]
                 message = "\\".join(l[1:])
-                print(pseudo+": "+message)
+                fen.receive(pseudo+": "+message)
             elif msg[:3] == "ERR":
                 pass
-        print("sortie du getter")
+        print("Fin du fetcher")
     def stop(self):
         self.continuer = False
+
+def command_interpreter(s):
+    if s[0] != "/":
+        return "MSG "+s
+    if s[1:5].lower() == "nick":
+        return "NIK "+s[6:]
+    else:
+        return None
+
+class Fenetre():
+    def __init__(self):
+        self.win = tk.Tk()
+        self.win.grid_rowconfigure(0, weight=1)
+        self.win.grid_columnconfigure(0, weight=1)
+        self.win.title('Chat PA-10')
+        self.menubar = tk.Menu(self.win)
+        self.filemenu = tk.Menu(self.menubar, tearoff=0)
+        self.menubar.add_cascade(label="File", menu=self.filemenu)
+        self.filemenu.add_command(label="Exit",command=self.win.destroy)
+        self.out_text = tk.Text(self.win)
+        #self.out_text.config(state=tk.DISABLED)
+        self.out_text.grid(row=0, sticky=tk.W+tk.E+tk.N+tk.S)
+        self.in_text = tk.Entry(self.win)
+        self.in_text.grid(row=1,sticky=tk.S+tk.W+tk.E)
+        self.in_text.bind('<Return>',self.send)
+        self.send_button = tk.Button(command = self.send,text="Send")
+        self.send_button.grid(row=1, sticky=tk.SE)
+    def run(self):
+        self.win.mainloop()
+
+    def send(self, event = ''):
+        s=self.in_text.get()
+        self.in_text.delete(0,tk.END)
+        if len(s) == 0:
+            return
+        s = command_interpreter(s)
+        if s == None:
+            self.receive('invalid command')
+        else:
+            connection.send(s.encode())
+
+    def receive(self, msg):
+        print(msg)
+        #self.out_text.config(state=tk.ENABLED)
+        self.out_text.insert(tk.END,msg+"\n")
+        #self.out_text.config(state=tk.DISABLED)
+
+
 
 
 
@@ -51,32 +95,19 @@ if len(replying) == 0:
     print("Aucune réponse du serveur !")
     sys.exit(0)
 else:
-    print("a")
     reply = replying[0].recv(SIZE)
-    print("b")
 if reply != b"HELLO":
     print("Réponse invalide du serveur: "+reply.decode())
     sys.exit(0)
 print("Connection réussie")
-print("Choisir le pseudo:")
-if sys.version_info.major == 2:
-    msg = raw_input("> ")
-else:
-    msg = input("> ")
-s = "NIK "+msg
-s = s.encode()
-connection.send(s)
-getter = Getter(connection)
-getter.start()
-msg = ""
-while True:
-    if sys.version_info.major == 2:
-        msg = raw_input("> ")
-    else:
-        msg = input("> ")
-    if msg == "exit":
-        break
-    connection.send(b"MSG "+msg.encode())
-getter.stop()
-getter.join()
+fetcher = Fetcher(connection)
+fetcher.start()
+
+
+fen = Fenetre()
+fen.run()
+
+
+fetcher.stop()
+fetcher.join()
 connection.close()
